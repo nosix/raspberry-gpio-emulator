1aecfb536053eb78c0caadf7240c55cf9a679a14
nosix <nosix@users.noreply.github.com>
2020-04-16 15:00:37 +0900

    docs: change author email

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='RPi.GPIO emulator',
     license=__license__,
     author=__author__,
-    author_email='nosix@anyspirit.org',
+    author_email='nosix@funmake.jp',
     url='http://nosix.github.io/raspberry-gpio-emulator/',
     keywords='raspberry pi gpio emulator python3',
     packages=find_packages(),
